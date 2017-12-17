fc01dea35c18e4aeb78b7022298190d15bc8f79d
Nathan Leach <nathanoleach@gmail.com>
2017-12-16 22:48:17 -0500

    alter for https

--- lib/osrm_api/request/base_request.rb
+++ lib/osrm_api/request/base_request.rb
@@ -29,13 +29,24 @@ module OSRM
       # @param [String] host
       # @param [Fixnum] port
       # @return [URI]
+      # https://router.project-osrm.org/table/v1/driving/-70.4429342,43.6796943;-71.0595678,42.3604823
+      # client = OSRM::Client.new(host: 'router.project-osrm.org', port: 443)
+      # distance = client.distance('40.723279,-73.937766', '40.90,-73.10', '40.82279,-73.937766')
       def build_uri(host, port)
-        URI::HTTP.build(
+        locations = ''
+        params.each do |param|
+          #locations += param[1].to_s + ';'
+          if !locations.blank?
+            locations += ";"
+          end
+          locations += params.first[1].split(",")[1].to_s + "," + params.first[1].split(",")[0].to_s
+        end
+        #byebug
+        URI::HTTPS.build(
             host: host,
-            port: port.to_i,
-            path: "/#{service}",
-            query: URI.encode_www_form(params +
-                       default_params.map { |key, item| [key, item] })
+            #port: port.to_i,
+            path: "/#{service}/v1/driving/" + locations #,
+            # query: locations #(params + default_params.map { |key, item| [key, item] })
         )
       end
     end
